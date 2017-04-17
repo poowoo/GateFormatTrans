2e4ae0b6f4bfc450a80a1154ad6eaa252dd5bb6e
david_hung <david_hung@cyberon.com.tw>
2017-04-17 17:47:52 +0800

    infomation save in nodes_list

--- francetoxml.py
+++ francetoxml.py
@@ -8,24 +8,27 @@ from xml import xml_write_gatedocument_end
 from xml import xml_write_gatedocument_feature
 from xml import xml_write_textwithnodes
 from xml import xml_write_annotation_set
+from xml import xml_set_nodes
 
 def file_xml_format(wor,pos,lia,out_fn):	
 	#print annotation_set
-	node_index_list = []
+	nodes_list = []
 
 	xml_write_header(out_fn)
 	xml_write_gatedocument_begin(out_fn)
 	xml_write_gatedocument_feature(out_fn,os.path.abspath(sys.argv[1]))
-	node_index_list = xml_write_textwithnodes(out_fn,wor)
-	xml_write_annotation_set(out_fn,wor,pos,lia,node_index_list)
+	#nodes_list = xml_write_textwithnodes(out_fn,wor)
+	nodes_list = xml_set_nodes(wor,pos,lia)
+	xml_write_textwithnodes(out_fn,nodes_list)
+	xml_write_annotation_set(out_fn,nodes_list)
 	xml_write_gatedocument_end(out_fn)
 	#out.close()
 
 def read_fixed_format_txt(fn):
 
 	wor=[] #word
-	pos=[]	#part of speech 
-	lia=[] #liasion
+	pos=[] #part of speech 
+	lia=[] #liaision
 
 	with open(fn, mode='r', encoding='UTF-8') as file:
 
@@ -45,5 +48,5 @@ def read_fixed_format_txt(fn):
 
 if __name__ == "__main__":
 
-	lang_word,lang_pos,lang_liasion = read_fixed_format_txt(sys.argv[1])
-	file_xml_format(lang_word,lang_pos,lang_liasion,sys.argv[2])
+	lang_word,lang_pos,lang_liaision = read_fixed_format_txt(sys.argv[1])
+	file_xml_format(lang_word,lang_pos,lang_liaision,sys.argv[2])
--- xml.py
+++ xml.py
@@ -1,4 +1,13 @@
 # -*- coding: utf8 -*-
+from enum import Enum
+class node_info(Enum):
+	index = 0
+	word = 1
+	start = 2
+	end = 3
+	pos = 4
+	liaison = 5
+
 def xml_write_header(fn):
 
 	out = open(fn, mode='w', encoding='UTF-8')
@@ -36,77 +45,104 @@ def xml_write_gatedocument_feature(fn,abspath):
 	out.write("</GateDocumentFeatures>\n")
 	out.close()
 
-def xml_write_textwithnodes(fn,words):
-
-	node_index_l = []
-	node_index = 0
+def xml_write_textwithnodes(fn,nodes_list):
+	
 	out = open(fn, mode='a', encoding='UTF-8')	
 	out.write("<TextWithNodes>")
-	for word in words:
-		if word is '\n':
+	for node in nodes_list:
+		if node[node_info.word.value] is "\n":
 			out.write("&#xd;\n")
 		else:			
-			out.write("<Node id=\""+str(node_index)+"\"/>")
-			node_index_l.append(node_index)
-			node_index += len(word)
-			out.write(word+"<Node id=\""+str(node_index)+"\"/> ")
-			node_index_l.append(node_index)
-			node_index += 1			
+			out.write("<Node id=\""+str(node[node_info.start.value])+"\"/>"+node[node_info.word.value]+"<Node id=\""+str(node[node_info.end.value])+"\"/> ")
 	out.write("</TextWithNodes>\n")
-	out.close()
-	return node_index_l
+	out.close()	
 
-def xml_write_annotation_set(fn,word,pos,liasion,node_index_list):
-	
-	node_index = 0	
+def xml_write_annotation_set(fn,nodes_list):
+		
 	anno_index = 0
-	liasion_type =""
-	liasion_boolean = ""
-	next_line_count = 0
+	liaison_type =""		
 	out = open(fn, mode='a', encoding='UTF-8')
 	out.write("<AnnotationSet>\n")
-	#xml pos
-	for p in pos:
-		if p is not "NL":		
-			out.write("<Annotation Id=\""+str(anno_index)+"\" Type=\""+p+"\" StartNode=\""+str(node_index_list[node_index])+"\" EndNode=\""+str(node_index_list[node_index+1])+"\">\n")
+	#xml word
+	for node in nodes_list:
+		if node[node_info.word.value] is not "\n":		
+			out.write("<Annotation Id=\""+str(anno_index)+"\" Type=\"word\" StartNode=\""+str(node[node_info.start.value])+"\" EndNode=\""+str(node[node_info.end.value])+"\">\n")
+			out.write("<Feature>\n")
+			out.write("  <Name className=\"java.lang.String\">string</Name>\n")
+			out.write("  <Value className=\"java.lang.String\">"+node[node_info.word.value]+"</Value>\n")
+			out.write("</Feature>\n")
 			out.write("<Feature>\n")
 			out.write("  <Name className=\"java.lang.String\">part of speech</Name>\n")
-			out.write("  <Value className=\"java.lang.String\">"+p+"</Value>\n")
+			out.write("  <Value className=\"java.lang.String\">"+node[node_info.pos.value]+"</Value>\n")
 			out.write("</Feature>\n")
-			out.write("</Annotation>\n")
-			node_index += 2
+			out.write("<Feature>\n")
+			out.write("  <Name className=\"java.lang.String\">liaison</Name>\n")
+			out.write("  <Value className=\"java.lang.Boolean\">"+node[node_info.liaison.value]+"</Value>\n")
+			out.write("</Feature>\n")
+			out.write("</Annotation>\n")			
+			anno_index += 1
+	#xml pos
+	for node in nodes_list:
+		if node[node_info.word.value] is not "\n":		
+			out.write("<Annotation Id=\""+str(anno_index)+"\" Type=\""+node[node_info.pos.value]+"\" StartNode=\""+str(node[node_info.start.value])+"\" EndNode=\""+str(node[node_info.end.value])+"\">\n")
+			out.write("<Feature>\n")
+			out.write("  <Name className=\"java.lang.String\">part of speech</Name>\n")
+			out.write("  <Value className=\"java.lang.String\">"+node[node_info.pos.value]+"</Value>\n")
+			out.write("</Feature>\n")
+			out.write("</Annotation>\n")			
 			anno_index += 1
 	
-	node_index = 0
-	#xml liasion
-	for l in liasion:
-		if l is not "N":
-			if l is "T":
-				liasion_type = "Liasion_True"
-				liasion_boolean = "true"
+	#xml liaison
+	for node in nodes_list:
+		if node[node_info.word.value] is not "\n":
+			if node[node_info.liaison.value] is "True":
+				liaison_type = "liaison_True"				
 			else:
-				liasion_type = "Liasion_False"
-				liasion_boolean = "false"
-			out.write("<Annotation Id=\""+str(anno_index)+"\" Type=\""+liasion_type+"\" StartNode=\""+str(node_index_list[node_index])+"\" EndNode=\""+str(node_index_list[node_index+1])+"\">\n")
+				liaison_type = "liaison_False"				
+			out.write("<Annotation Id=\""+str(anno_index)+"\" Type=\""+liaison_type+"\" StartNode=\""+str(node[node_info.start.value])+"\" EndNode=\""+str(node[node_info.end.value])+"\">\n")
 			out.write("<Feature>\n")
-			out.write("  <Name className=\"java.lang.String\">liasion</Name>\n")
-			out.write("  <Value className=\"java.lang.Boolean\">"+liasion_boolean+"</Value>\n")
+			out.write("  <Name className=\"java.lang.String\">liaison</Name>\n")
+			out.write("  <Value className=\"java.lang.Boolean\">"+node[node_info.liaison.value]+"</Value>\n")
 			out.write("</Feature>\n")
-			out.write("</Annotation>\n")
-			node_index += 2
+			out.write("</Annotation>\n")			
 			anno_index += 1
 	#xml special case
 
-	for i in range(len(pos)):
-		if pos[i] is not "NL":
-			if pos[i] is "V" and pos[i+1] is "N" and liasion[i+1] is "T":
-				out.write("<Annotation Id=\""+str(anno_index)+"\" Type=\"V+N and liasion ture\" StartNode=\""+str(node_index_list[(i-next_line_count)*2])+"\" EndNode=\""+str(node_index_list[(i-next_line_count)*2+3])+"\">\n")			
-				out.write("</Annotation>\n")						
-			elif pos[i] is "V" and pos[i+1] is "N" and liasion[i+1] is "F":
-				out.write("<Annotation Id=\""+str(anno_index)+"\" Type=\"V+N and liasion false\" StartNode=\""+str(node_index_list[(i-next_line_count)*2])+"\" EndNode=\""+str(node_index_list[(i-next_line_count)*2+3])+"\">\n")			
-				out.write("</Annotation>\n")			
-			anno_index += 1
-		else:
-			next_line_count += 1	
+	#for i in range(len(pos)):
+	#	if pos[i] is not "NL":
+	#		if pos[i] is "V" and pos[i+1] is "N" and liaison[i+1] is "T":
+	#			out.write("<Annotation Id=\""+str(anno_index)+"\" Type=\"V+N and liaison ture\" StartNode=\""+str(node_index_list[(i-next_line_count)*2])+"\" EndNode=\""+str(node_index_list[(i-next_line_count)*2+3])+"\">\n")			
+	#			out.write("</Annotation>\n")						
+	#		elif pos[i] is "V" and pos[i+1] is "N" and liaison[i+1] is "F":
+	#			out.write("<Annotation Id=\""+str(anno_index)+"\" Type=\"V+N and liaison false\" StartNode=\""+str(node_index_list[(i-next_line_count)*2])+"\" EndNode=\""+str(node_index_list[(i-next_line_count)*2+3])+"\">\n")			
+	#			out.write("</Annotation>\n")			
+	#		anno_index += 1
+	#	else:
+	#		next_line_count += 1	
 	out.write("</AnnotationSet>\n")
 	out.close()
+
+def xml_set_nodes(words,pos,liaison):
+
+	nodes_list = []
+	#node = [] # node index, word, start_index , end_index , pos , liaison 
+	node_index = 0
+	for i in range(len(words)):
+		node = []
+		if words[i] is not "\n" and pos[i] is not "NL" and liaison[i] is not "N":			
+			node.append(i)
+			node.append(words[i])
+			node.append(node_index)
+			node_index += len(words[i])
+			node.append(node_index)
+			node_index += 1
+			node.append(pos[i])
+			if liaison[i] is "T":
+				node.append("True")
+			else:
+				node.append("False")
+		else:
+			node.append(i)
+			node.append("\n")
+		nodes_list.append(node)
+	return nodes_list
